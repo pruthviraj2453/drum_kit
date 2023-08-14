5921282c3fc5e372904b7f5e7f3e8c766353ae3e
Pruthviraj Dodiya <pruthvirajdodiya@Pruthvirajs-MacBook-Air.local>
2023-08-14 10:50:30 +0530

    Drum kit project

---
+++ index.js
@@ -0,0 +1,58 @@
+//detecting button press 
+var len=document.querySelectorAll(".drum").length;
+for(var i=0;i<len;i++){
+    document.querySelectorAll(".drum")[i].addEventListener('click',function(){
+        var buttonInnerHtml = this.innerHTML;
+        keyPressed(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    });
+}
+
+//Detecting keyboard press
+
+document.addEventListener('keydown',function(event){
+    keyPressed(event.key);
+    buttonAnimation(event.key);
+})
+
+function keyPressed(key){
+    switch (key) {
+     case "w":
+         var crash = new Audio('sounds/crash.mp3');
+         crash.play();   
+     break
+     case "a":
+         var kick = new Audio('sounds/kick-bass.mp3');
+         kick.play();   
+     break;
+     case "s":
+         var snare = new Audio('sounds/snare.mp3');
+         snare.play();   
+     break;
+     case "d":
+         var tom1 = new Audio('sounds/tom-1.mp3');
+         tom1.play();   
+     break;
+     case "j":
+         var tom2 = new Audio('sounds/tom-2.mp3');
+         tom2.play();   
+     break;
+     case "k":
+         var tom3 = new Audio('sounds/tom-3.mp3');
+         tom3.play();   
+     break;
+     case "l":
+         var tom4 = new Audio('sounds/tom-4.mp3');
+         tom4.play();   
+     break;
+     default:
+    }
+ }
+
+ function buttonAnimation(key){
+    var activeButton = document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100)
+ }
